68e042d039a315b570eb1ca17c43acfa81a46e90
Jayaraj Awasthi <awasthijayaraj11@gmail.com>
2025-08-01 08:25:29 +0545

    complete hero section with links to modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
+import HeroSection from './components/HeroSection'
 
 function App() {
 
 
   return (
     <>
-     
+      <HeroSection />
     </>
   )
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ const navigation = [
   { name: "Product", href: "#" },
   { name: "Features", href: "#" },
   { name: "MarketPlace", href: "#" },
-  { name: "Company", href: "#" },
+  // { name: "Company", href: "#" },
 ];
 
 export default function HeroSection() {
@@ -111,26 +111,26 @@ export default function HeroSection() {
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
-            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
+            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#fff] to-[#fff] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-4xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{" "}
+              Heads up! smart auto-detection is launching soon.{" "}
               <a href="#" className="font-semibold text-indigo-600">
                 <span aria-hidden="true" className="absolute inset-0" />
-                Read more <span aria-hidden="true">&rarr;</span>
+                Get notified <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
           </div>
           <div className="text-center">
-            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-              Data to enrich your online business
+            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-6xl">
+              Blur private information in images <br /> — with precision and privacy
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
+             Easily blur faces, license plates, and sensitive details — all in your browser.
+No uploads. Your images stay private.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
@@ -154,7 +154,7 @@ export default function HeroSection() {
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
-            className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
+            className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#fff] to-[#fff] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
           />
         </div>
       </div>
